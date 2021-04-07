b193d3dc7496beea7c2bd0a30b3dcdb9dd0c1216
Eugenia <costamariaeugenia1@gmail.com>
2021-04-07 16:35:59 -0300

    Update clase1-ejercicio2.py

--- clase1/clase1-ejercicio2.py
+++ clase1/clase1-ejercicio2.py
@@ -7,8 +7,9 @@ n = float(input("Por favor ingrese un número, para poder hacer cálculos: "))
 # Caclulo el cuadrado y el cubo del número ingresado
 n_al_cuadrado = n*n
 n_al_cubo = n*n*n
+resultado = n + n_al_cuadrado + n_al_cubo
 
 # Muestro los cálculos
-print(f'El cuadrado del número ingresado es: {n_al_cuadrado} y el cubo del número ingresado es {n_al_cubo}')
+print(f'El resultado del número ingresado, sumado el número al cuadrado y el número al cubo es:  {resultado}')
 
 
