3070f8005acbc0a1d6c25edd87f1b5e4f1efba32
Eugenia <costamariaeugenia1@gmail.com>
2021-06-09 09:18:14 -0300

    Delete forms.py

--- clase10_django/PROJECTCLASE11/AEROLINEA/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Vuelo
-
-class FormVueloCustom(forms.ModelForm):
-    #campos del modelo
-    class Meta:
-        model = Vuelo
-        fields = ('origen', 'destino', 'duracion')
