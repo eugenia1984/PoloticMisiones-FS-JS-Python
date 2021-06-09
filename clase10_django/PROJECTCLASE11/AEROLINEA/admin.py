2154dae09ef216bbd09c49ec91aa6c751f0c7788
Eugenia <costamariaeugenia1@gmail.com>
2021-06-09 09:17:52 -0300

    Delete admin.py

--- clase10_django/PROJECTCLASE11/AEROLINEA/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Vuelo, Aeropuerto, Pasajero
-
-# Register your models here.
-admin.site.register(Vuelo)
-admin.site.register(Aeropuerto)
-admin.site.register(Pasajero)
